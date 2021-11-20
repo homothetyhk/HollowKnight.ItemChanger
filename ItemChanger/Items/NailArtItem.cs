9a79cf45d34fc7af1893630774712ab1b5c0308a
homothetyhk <homothetyhk@gmail.com>
2021-11-20 11:58:56 -0800

    Add NailArtItem.

---
+++ ItemChanger/Items/NailArtItem.cs
@@ -0,0 +1,17 @@
+﻿namespace ItemChanger.Items
+{
+    public class NailArtItem : BoolItem
+    {
+        public override void GiveImmediate(GiveInfo info)
+        {
+            base.GiveImmediate(info);
+            PlayerData.instance.SetBool(nameof(PlayerData.hasNailArt), true);
+            if (PlayerData.instance.GetBool(nameof(PlayerData.hasCyclone)) 
+                && PlayerData.instance.GetBool(nameof(PlayerData.hasDashSlash))
+                && PlayerData.instance.GetBool(nameof(PlayerData.hasUpwardSlash)))
+            {
+                PlayerData.instance.SetBool(nameof(PlayerData.hasAllNailArts), true);
+            }
+        }
+    }
+}
