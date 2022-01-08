dcded33447dd0a69adfc6c4171a4cbc245f56db5
homothetyhk <homothetyhk@gmail.com>
2022-01-07 23:29:25 -0800

    Update version.

--- ItemChanger/ItemChangerMod.cs
+++ ItemChanger/ItemChangerMod.cs
@@ -158,7 +158,40 @@ namespace ItemChanger
 
         public override string GetVersion()
         {
-            return "1.0.1";
+            return _version;
+        }
+
+        private static readonly string _sha1;
+        private static readonly string _version;
+        static ItemChangerMod()
+        {
+            System.Reflection.Assembly a = typeof(ItemChangerMod).Assembly;
+
+            using var sha1 = System.Security.Cryptography.SHA1.Create();
+            using var sr = File.OpenRead(a.Location);
+            _sha1 = Convert.ToBase64String(sha1.ComputeHash(sr));
+
+            int buildHash;
+            unchecked // stable string hash code
+            {
+                int hash1 = 5381;
+                int hash2 = hash1;
+                string str = _sha1;
+
+                for (int i = 0; i < str.Length && str[i] != '\0'; i += 2)
+                {
+                    hash1 = ((hash1 << 5) + hash1) ^ str[i];
+                    if (i == str.Length - 1 || str[i + 1] == '\0')
+                        break;
+                    hash2 = ((hash2 << 5) + hash2) ^ str[i + 1];
+                }
+
+                buildHash = hash1 + (hash2 * 1566083941);
+                buildHash = Math.Abs(buildHash) % 997;
+            }
+
+            Version v = a.GetName().Version;
+            _version = $"{v.Major}.{v.Minor}.{v.Build}+{buildHash.ToString().PadLeft(3, '0')}";
         }
 
         public override int LoadPriority() => -2;
