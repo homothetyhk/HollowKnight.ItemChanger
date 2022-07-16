51027f24fffad8eed0f7a7bcd8115fe44676e114
homothetyhk <homothetyhk@gmail.com>
2022-07-15 21:39:48 -0700

    Disable darkness regions in scenes modified by DarknessLevelEditModule.

--- ItemChanger/Modules/DarknessLevelEditModule.cs
+++ ItemChanger/Modules/DarknessLevelEditModule.cs
@@ -4,6 +4,7 @@ namespace ItemChanger.Modules
 {
     /// <summary>
     /// Module for changing the darkness level of a scene. Permitted values are -1, 0 (normal), 1, 2 (fully dark)
+    /// <br/>Darkness regions in modified scenes will be disabled.
     /// </summary>
     public class DarknessLevelEditModule : Module
     {
@@ -12,11 +13,13 @@ namespace ItemChanger.Modules
         public override void Initialize()
         {
             On.SceneManager.Start += OnSceneManagerStart;
+            Events.AddFsmEdit(new("Darkness Region"), DisableDarknessRegion);
         }
 
         public override void Unload()
         {
             On.SceneManager.Start -= OnSceneManagerStart;
+            Events.RemoveFsmEdit(new("Darkness Region"), DisableDarknessRegion);
         }
 
         private void OnSceneManagerStart(On.SceneManager.orig_Start orig, SceneManager self)
@@ -26,6 +29,11 @@ namespace ItemChanger.Modules
                 self.darknessLevel = darknessLevel;
             }
             orig(self);
+        }
+
+        private void DisableDarknessRegion(PlayMakerFSM fsm)
+        {
+            if (darknessLevelsByScene.ContainsKey(fsm.gameObject.scene.name)) fsm.GetState("Init").ClearTransitions();
         }
     }
 }
