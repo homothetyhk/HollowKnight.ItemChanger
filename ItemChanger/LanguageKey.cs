2a5dc95f74e37b19e39331856a849c44cc736694
homothetyhk <homothetyhk@gmail.com>
2022-07-15 21:27:17 -0700

    Add LanguageEditModule.

--- ItemChanger/LanguageKey.cs
+++ ItemChanger/LanguageKey.cs
@@ -1,4 +1,6 @@
-﻿namespace ItemChanger
+﻿using Newtonsoft.Json;
+
+namespace ItemChanger
 {
     public readonly struct LanguageKey
     {
@@ -34,6 +36,24 @@
         public override string ToString()
         {
             return Sheet == null ? Key : $"{Sheet}-{Key}";
+        }
+
+        public class LanguageKeyDictConverter<TValue> : JsonConverter<Dictionary<LanguageKey, TValue>>
+        {
+            public override Dictionary<LanguageKey, TValue> ReadJson(
+                JsonReader reader,
+                Type objectType,
+                Dictionary<LanguageKey, TValue> existingValue,
+                bool hasExistingValue,
+                JsonSerializer serializer)
+            {
+                return serializer.Deserialize<KeyValuePair<LanguageKey, TValue>[]>(reader).ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+            }
+
+            public override void WriteJson(JsonWriter writer, Dictionary<LanguageKey, TValue> value, JsonSerializer serializer)
+            {
+                serializer.Serialize(writer, value.ToArray());
+            }
         }
     }
 }
---
+++ ItemChanger/Modules/LanguageEditModule.cs
@@ -0,0 +1,92 @@
+﻿using Newtonsoft.Json;
+
+namespace ItemChanger.Modules
+{
+    /// <summary>
+    /// Module for packaging language edits with an ItemChanger save.
+    /// </summary>
+    public class LanguageEditModule : Module
+    {
+        public Dictionary<LanguageKey, LanguageEdit> languageEdits = new();
+
+        public override void Initialize()
+        {
+            foreach (LanguageEdit e in languageEdits.Values) e.Hook();
+        }
+
+        public override void Unload()
+        {
+            foreach (LanguageEdit e in languageEdits.Values) e.Unhook();
+        }
+
+        public void AddLanguageEdit(LanguageKey key, string value, bool ignoreExistingOverrides = false)
+        {
+            if (languageEdits.TryGetValue(key, out LanguageEdit oldValue))
+            {
+                oldValue.Unhook();
+            }
+            languageEdits[key] = new()
+            {
+                Sheet = key.Sheet,
+                Key = key.Key,
+                Value = value,
+                IgnoreExistingOverrides = ignoreExistingOverrides,
+            };
+        }
+
+        public void RemoveLanguageEdit(LanguageKey key)
+        {
+            if (languageEdits.TryGetValue(key, out LanguageEdit oldValue))
+            {
+                oldValue.Unhook();
+            }
+            languageEdits.Remove(key);
+        }
+
+        public record LanguageEdit
+        {
+            public string Sheet { get; init; }
+            public string Key { get; init; }
+            public string Value { get; init; }
+            /// <summary>
+            /// If false, and the key has been set to a nonempty value in a language.xml included with ItemChanger, then the language edit will not run.
+            /// </summary>
+            public bool IgnoreExistingOverrides { get; init; }
+
+            public LanguageKey LanguageKey { get => new(Sheet, Key); }
+
+            public void Hook()
+            {
+                Events.AddLanguageEdit(LanguageKey, Edit);
+            }
+
+            public void Unhook()
+            {
+                Events.AddLanguageEdit(LanguageKey, Edit);
+            }
+
+            public void Edit(ref string value)
+            {
+                if (!IgnoreExistingOverrides)
+                {
+                    if (string.IsNullOrEmpty(Internal.LanguageStringManager.GetICString(Key, Sheet))) return;
+                }
+
+                value = Value;
+            }
+        }
+
+        public class LanguageEditDictConverter : JsonConverter<Dictionary<LanguageKey, LanguageEdit>>
+        {
+            public override Dictionary<LanguageKey, LanguageEdit> ReadJson(JsonReader reader, Type objectType, Dictionary<LanguageKey, LanguageEdit> existingValue, bool hasExistingValue, JsonSerializer serializer)
+            {
+                return serializer.Deserialize<List<LanguageEdit>>(reader).ToDictionary(e => e.LanguageKey);
+            }
+
+            public override void WriteJson(JsonWriter writer, Dictionary<LanguageKey, LanguageEdit> value, JsonSerializer serializer)
+            {
+                serializer.Serialize(writer, value?.Values?.ToList());
+            }
+        }
+    }
+}
