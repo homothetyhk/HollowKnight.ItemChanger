0d352b7351c59a66c9e106cdc10b97abc0f4650e
homothetyhk <homothetyhk@gmail.com>
2022-07-17 10:08:34 -0700

    Another swim overhaul: revert surface height while keeping fix to prevent seam jumps.

--- ItemChanger/Modules/SwimSkill.cs
+++ ItemChanger/Modules/SwimSkill.cs
@@ -16,7 +16,6 @@ namespace ItemChanger.Modules
         public override void Initialize()
         {
             Events.AddFsmEdit(new("Surface Water Region"), EditWaterSurface);
-            Events.AddFsmEdit(new("Splash Surface", "Corpse Splash"), AddHazardSplash);
             Modding.ModHooks.GetPlayerBoolHook += SkillBoolGetOverride;
             Modding.ModHooks.SetPlayerBoolHook += SkillBoolSetOverride;
         }
@@ -24,7 +23,6 @@ namespace ItemChanger.Modules
         public override void Unload()
         {
             Events.RemoveFsmEdit(new("Surface Water Region"), EditWaterSurface);
-            Events.RemoveFsmEdit(new("Splash Surface", "Corpse Splash"), AddHazardSplash);
             Modding.ModHooks.GetPlayerBoolHook -= SkillBoolGetOverride;
             Modding.ModHooks.SetPlayerBoolHook -= SkillBoolSetOverride;
         }
@@ -54,67 +52,23 @@ namespace ItemChanger.Modules
         {
             if (fsm.gameObject.LocateMyFSM("Acid Armour Check") != null) return; // acid
 
-            Transform splash = fsm.transform.Find("Splash Surface");
-            splash.gameObject.layer = 17;
+            GameObject splashSurface = fsm.transform.Find("Splash Surface").gameObject;
+            splashSurface.layer = 17; // orig is 8, which can enable seam jumping when it intersects with other terrain colliders
+            splashSurface.AddComponent<NonBouncer>();
 
-            DamageHero dh = splash.gameObject.AddComponent<DamageHero>();
-            dh.damageDealt = 1;
-            dh.hazardType = (int)HazardType.ACID;
-            splash.gameObject.AddComponent<NonBouncer>();
-
-            void DoActivateSwim()
-            {
-                dh.damageDealt = 0;
-            }
-
-            FsmState checkSwim = fsm.AddState("Check Swim");
-            FsmState activateSwim = fsm.AddState("Activate Swim");
             FsmState idle = fsm.GetState("Idle");
-            foreach (FsmTransition t in fsm.FsmStates.SelectMany(s => s.Transitions))
-            {
-                if (t.ToFsmState == idle || t.ToState == "Idle") t.SetToState(checkSwim);
-            }
-            checkSwim.AddTransition("SWIM GET", activateSwim);
-            checkSwim.AddFirstAction(new DelegateBoolTest(() => canSwim, "SWIM GET", null));
-            activateSwim.AddFirstAction(new Lambda(DoActivateSwim));
-            activateSwim.AddTransition(FsmEvent.Finished, idle);
-        }
-
-        private void AddHazardSplash(PlayMakerFSM fsm)
-        {
-            FsmState detect = fsm.GetState("Detect");
-            FsmState splash = fsm.GetState("Splash");
             FsmState checkSwim = fsm.AddState("Check Swim");
+            FsmState damageHero = fsm.AddState("Damage Hero");
+            FsmState bigSplash = fsm.GetState("Big Splash?");
 
-            // corpse splash seems to always be black, so let's fix that
-            if (fsm.transform.parent?.gameObject?.LocateMyFSM("Surface Water Region") is PlayMakerFSM swr)
-            {
-                bool black = swr.FsmVariables.FindFsmBool("Black").Value;
-                if (!black)
-                {
-                    FsmState state = swr.GetState("Blue");
-                    splash.GetFirstActionOfType<SpawnObjectFromGlobalPool>().gameObject.Value
-                        = state.GetFirstActionOfType<SetGameObject>().gameObject.Value;
-                    splash.GetFirstActionOfType<FlingObjectsFromGlobalPool>().gameObject.Value
-                        = state.GetActionsOfType<SetGameObject>()[1].gameObject.Value;
-                }
-            }
+            idle.Transitions[0].SetToState(checkSwim);
+            checkSwim.AddFirstAction(new DelegateBoolTest(() => canSwim, "SWIM", "DAMAGE"));
+            checkSwim.AddTransition("SWIM", bigSplash);
+            checkSwim.AddTransition("DAMAGE", damageHero);
 
-            Trigger2dEventLayer t = detect.GetFirstActionOfType<Trigger2dEventLayer>();
-            FsmGameObject fgo = t.storeCollider;
-            detect.AddLastAction(new Trigger2dEvent
-            {
-                collideLayer = new(""),
-                collideTag = "HeroBox",
-                sendEvent = FsmEvent.GetFsmEvent("CHECK"),
-                storeCollider = fgo,
-                trigger = t.trigger,
-            });
-            detect.AddTransition("CHECK", checkSwim);
-
-            checkSwim.AddFirstAction(new DelegateBoolTest(() => !canSwim, "SPLASH", "CANCEL"));
-            checkSwim.AddTransition("CANCEL", detect);
-            checkSwim.AddTransition("SPLASH", splash);
+            damageHero.Actions = fsm.GetState("Splash In Norm").Actions.Where(a => a is not SetPosition).ToArray(); // play splash audio and fling splash particles
+            damageHero.AddLastAction(new Lambda(() => HeroController.instance.TakeDamage(fsm.gameObject, CollisionSide.bottom, 1, 2)));
+            damageHero.AddTransition(FsmEvent.Finished, idle);
         }
     }
 }
