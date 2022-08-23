79e669863c2e950f92f1e2aa69c08f699e4a1484
homothetyhk <homothetyhk@gmail.com>
2022-08-23 16:09:58 -0700

    Disable QoL SkipCutscenes for KingsBrandLocation.

--- ItemChanger/Locations/SpecialLocations/KingsBrandLocation.cs
+++ ItemChanger/Locations/SpecialLocations/KingsBrandLocation.cs
@@ -1,4 +1,6 @@
-﻿namespace ItemChanger.Locations.SpecialLocations
+﻿using System.Reflection;
+
+namespace ItemChanger.Locations.SpecialLocations
 {
     /// <summary>
     /// ObjectLocation which prevents the King's Brand avalanche sequence from occuring.
@@ -10,6 +12,16 @@
             base.OnLoad();
             Events.AddFsmEdit(sceneName, new("Avalanche", "Activate"), Destroy);
             Events.AddFsmEdit(sceneName, new("Avalanche End", "Control"), Destroy);
+            try
+            {
+                Type.GetType("QoL.SettingsOverride, QoL")
+                    ?.GetMethod("OverrideSettingToggle", BindingFlags.Public | BindingFlags.Static)
+                    ?.Invoke(null, new object[] { "SkipCutscenes", "AfterKingsBrandGet", false });
+            }
+            catch (Exception e)
+            {
+                LogError(e);
+            }
         }
 
         protected override void OnUnload()
@@ -17,6 +29,16 @@
             base.OnUnload();
             Events.RemoveFsmEdit(sceneName, new("Avalanche", "Activate"), Destroy);
             Events.RemoveFsmEdit(sceneName, new("Avalanche End", "Control"), Destroy);
+            try
+            {
+                Type.GetType("QoL.SettingsOverride, QoL")
+                    ?.GetMethod("RemoveSettingOverride", BindingFlags.Public | BindingFlags.Static)
+                    ?.Invoke(null, new object[] { "SkipCutscenes", "AfterKingsBrandGet" });
+            }
+            catch (Exception e)
+            {
+                LogError(e);
+            }
         }
 
         private void Destroy(PlayMakerFSM fsm) => UnityEngine.Object.Destroy(fsm.gameObject);
