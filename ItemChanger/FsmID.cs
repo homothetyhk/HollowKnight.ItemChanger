4aef0ac93d1f772afa1e97330165b0e7a05568a7
homothetyhk <homothetyhk@gmail.com>
2022-10-01 14:49:20 -0700

    Fix Watcher Knight conditional load, and document strong FsmID.

--- ItemChanger/FsmID.cs
+++ ItemChanger/FsmID.cs
@@ -1,4 +1,6 @@
-﻿namespace ItemChanger
+﻿using ItemChanger.Extensions;
+
+namespace ItemChanger
 {
     public readonly struct FsmID
     {
@@ -14,6 +16,7 @@
 
         /// <summary>
         /// Create an FsmID which matches any object with this ObjectName and FsmName.
+        /// <br/>An object name with a leading '/' will be interpreted as a path, and will only match objects with that exact path in hierarchy.
         /// </summary>
         [Newtonsoft.Json.JsonConstructor]
         public FsmID(string ObjectName, string FsmName)
@@ -26,6 +29,12 @@
         /// Create an FsmID which matches any object with the same ObjectName and FsmName as the fsm.
         /// </summary>
         public FsmID(PlayMakerFSM fsm) : this(fsm.gameObject.name, fsm.FsmName) { }
+
+        /// <summary>
+        /// Create an FsmID which matches any object with the same ObjectName and FsmName as the fsm.
+        /// <br/>If the strongID parameter is true, the ObjectName will only match objects with the exact same path in hierarchy as the input.
+        /// </summary>
+        public FsmID(PlayMakerFSM fsm, bool strongID) : this(strongID ? "/" + fsm.gameObject.transform.GetPathInHierarchy() : fsm.gameObject.name, fsm.FsmName) { }
 
         public readonly string ObjectName;
         public readonly string FsmName;
---
+++ ItemChanger/Modules/FixWatcherKnightConditionalLoad.cs
@@ -0,0 +1,39 @@
+﻿namespace ItemChanger.Modules
+{
+    /// <summary>
+    /// Default module which allows the Watcher Knight boss fight to load when entering the room from unexpected transitions and adjusts the gate trigger to allow entering the fight from the right.
+    /// </summary>
+    [DefaultModule]
+    public class FixWatcherKnightConditionalLoad : Module
+    {
+        public override void Initialize()
+        {
+            On.SceneAdditiveLoadConditional.OnEnable += SceneAdditiveLoadConditional_OnEnable;
+            Events.AddFsmEdit(SceneNames.Ruins2_03, new FsmID("Battle Control", "Battle Control"), MoveGateTrigger);
+        }
+
+        public override void Unload()
+        {
+            On.SceneAdditiveLoadConditional.OnEnable -= SceneAdditiveLoadConditional_OnEnable;
+            Events.RemoveFsmEdit(SceneNames.Ruins2_03, new FsmID("Battle Control", "Battle Control"), MoveGateTrigger);
+        }
+
+        private void SceneAdditiveLoadConditional_OnEnable(On.SceneAdditiveLoadConditional.orig_OnEnable orig, SceneAdditiveLoadConditional self)
+        {
+            if (self.sceneNameToLoad.StartsWith(SceneNames.Ruins2_03))
+            {
+                self.doorTrigger = string.Empty;
+            }
+            orig(self);
+        }
+
+        private void MoveGateTrigger(PlayMakerFSM fsm)
+        {
+            BoxCollider2D box = fsm.GetComponent<BoxCollider2D>();
+            Vector2 size = new(box.size.x - 0.6f, box.size.y);
+            Vector2 offset = new(box.offset.x - 0.6f, box.offset.y);
+            box.size = size;
+            box.offset = offset;
+        }
+    }
+}
