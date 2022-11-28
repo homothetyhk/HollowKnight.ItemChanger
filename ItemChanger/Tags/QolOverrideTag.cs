120e6e39a7e35db7c263dd257d276591f7c9aebe
Sean Dempsey <dempsey.sean@outlook.com>
2022-11-27 22:48:14 -0800

    Add QoL override tag

---
+++ ItemChanger/Tags/QolOverrideTag.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItemChanger.Tags
+{
+    /// <summary>
+    /// A tag which enables a QoL override for an individual setting in a module, or an entire module.
+    /// </summary>
+    public class QolOverrideTag : Tag
+    {
+        public string ModuleName;
+        public string SettingName;
+        public bool Enable;
+
+        public override void Load(object parent)
+        {
+            base.Load(parent);
+
+            try
+            {
+                if (ModuleName != null)
+                {
+                    Type settingsOverride = Type.GetType("QoL.SettingsOverride, QoL");
+                    if (SettingName == null)
+                    {
+                        settingsOverride
+                            ?.GetMethod("OverrideModuleToggle", BindingFlags.Public | BindingFlags.Static)
+                            .Invoke(null, new object[] { ModuleName, Enable });
+                    }
+                    else
+                    {
+                        settingsOverride
+                            ?.GetMethod("OverrideSettingToggle", BindingFlags.Public | BindingFlags.Static)
+                            .Invoke(null, new object[] { ModuleName, SettingName, Enable });
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                LogError(e);
+            }
+        }
+
+        public override void Unload(object parent)
+        {
+            base.Unload(parent);
+
+            try
+            {
+                if (ModuleName != null)
+                {
+                    Type settingsOverride = Type.GetType("QoL.SettingsOverride, QoL");
+                    if (SettingName == null)
+                    {
+                        settingsOverride
+                            ?.GetMethod("RemoveModuleOverride", BindingFlags.Public | BindingFlags.Static)
+                            ?.Invoke(null, new object[] { ModuleName });
+                    }
+                    else
+                    {
+                        settingsOverride
+                            ?.GetMethod("RemoveSettingOverride", BindingFlags.Public | BindingFlags.Static)
+                            ?.Invoke(null, new object[] { ModuleName, SettingName });
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                LogError(e);
+            }
+        }
+    }
+}
